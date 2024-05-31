88ba418b3480b909d6fb67389ec7d4405cd9e8b8
lxndrbnsv <abnosov@gmail.com>
2024-05-31 09:05:52 +0200

    upd

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("home.html", context={"request": request})
+    return templates.TemplateResponse(
+        "home.html", context={"request": request}
+    )
 
 
 @app.post("/tasks", status_code=201)
-def run_task(payload = Body(...)):
+def run_task(payload=Body(...)):
     task_type = payload["type"]
     task = create_task.delay(int(task_type))
     return JSONResponse({"task_id": task.id})
@@ -31,6 +32,6 @@ def get_status(task_id):
     result = {
         "task_id": task_id,
         "task_status": task_result.status,
-        "task_result": task_result.result
+        "task_result": task_result.result,
     }
     return JSONResponse(result)
